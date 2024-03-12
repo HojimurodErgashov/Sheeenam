15eadce61d13426fabc0c1fb09abd407e25b74a9
farobiy <farobiyfrontend@gmail.com>
2024-03-12 16:26:21 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheeenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+﻿//===============================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//===============================================
+
+using Xunit;
+
+namespace Sheeenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
